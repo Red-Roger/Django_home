c0fa7b00d44fbef44753d18babe618b1d681cc11
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2024-02-24 11:13:58 +0200

    ///

--- quotes/users/views.py
+++ quotes/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
+from django.contrib.auth import authenticate, login, logout
+import messages
 
 def signupuser(request):
-    """
+    
     if request.user.is_authenticated:
         return redirect(to='quoteapp:main')
 
@@ -15,6 +17,21 @@ def signupuser(request):
             return redirect(to='quoteapp:main')
         else:
             return render(request, 'users/signup.html', context={"form": form})
-    """
+    
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quoteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='quoteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
